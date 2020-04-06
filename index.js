ac5a3752c4db259f2ddb4b494e0074f2639f5f19
mariiacd13 <maria.cabden@gmail.com>
2020-04-06 19:35:03 +0200

    2 files modified

---
+++ index.js
@@ -0,0 +1,12 @@
+
+import './style.css';
+
+var visualization = d3plus.viz()
+.container("#viz1")
+.data([{"year": 1991, "name":"alpha", "value": 15},{"year": 1992, "name":"alpha", "value": 20},])
+.type("bar")
+.id("name")
+.x("year")
+.y("value")
+.draw()
+
